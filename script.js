edd4f0da14f476bb23e1a665dbce43a8aa526b77
Scott MacDonald <scottmacd88@yahoo.com>
2020-04-05 06:23:12 -0700

    Added ajax function, queryURL and API key. Tested and information is working.

--- script.js
+++ script.js
@@ -1,4 +1,24 @@
 $(document).ready(function () {
 
+    let APIKey = "9a936c558ee294585acc3927c01d451b";
+    let queryURL = "https://api.openweathermap.org/data/2.5/weather?" +
+    "q=Bujumbura,Burundi&appid=" + APIKey;
+
+    $("#searchBtn").on("click", function () {
+       
+        $
+        .ajax({
+            url: queryURL,
+            method: "GET"
+        })
+        .then(function (response) {
+            console.log(response);
+        });
+
+    });
+
+
+
+
 
 });
